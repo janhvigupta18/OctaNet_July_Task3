09f49644cf0a8a0580a464579d31fa7f96c4f1b3
jain_ritesh <jain07118@gmail.com>
2023-06-22 10:27:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const nav = document.getElementById('navbar');
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+
+if (bar){
+    bar.addEventListener('click', ()=>{
+        nav.classList.add('active');
+    })
+};
+
+if (close){
+    close.addEventListener('click', ()=>{
+        nav.classList.remove('active');
+    })
+};
+
+console.log('It is Working conddion')
